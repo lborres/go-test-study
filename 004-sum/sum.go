1153486e7ab9870fd7b4c565ad318429e6361c8c
Louis Borres <lborresbusiness@gmail.com>
2026-01-02 07:31:05 +0800

    feat: completed 004

--- 004-sum/sum.go
+++ 004-sum/sum.go
@@ -1,10 +1,36 @@
 package sum
 
-func Sum(input [5]int) int {
+func Sum(input []int) int {
 	sum := 0
+
 	for _, number := range input {
 		sum += number
 	}
 
 	return sum
+}
+
+func SumAll(input ...[]int) []int {
+	var sums []int
+
+	for _, num := range input {
+		sums = append(sums, Sum(num))
+	}
+
+	return sums
+}
+
+func SumAllTails(input ...[]int) []int {
+	var sums []int
+
+	for _, num := range input {
+		if len(num) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := num[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
 }
--- 004-sum/sum_test.go
+++ 004-sum/sum_test.go
@@ -1,16 +1,55 @@
 package sum
 
-import "testing"
+import (
+	"reflect"
+	"slices"
+	"testing"
+)
+
+func assertSum(t testing.TB, input []int, expected, actual int) {
+	if actual != expected {
+		t.Fatalf("expected %d given %v, got %d", expected, input, actual)
+	}
+}
 
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
-		numbers := [5]int{1, 2, 3, 4, 5}
+		numbers := []int{1, 2, 3, 4, 5}
 
 		sum := Sum(numbers)
 		expected := 15
 
-		if sum != expected {
-			t.Fatalf("expected %d given %v, got %d", expected, numbers, sum)
+		assertSum(t, numbers, expected, sum)
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		sum := Sum(numbers)
+		expected := 6
+
+		assertSum(t, numbers, expected, sum)
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("should collect sum", func(t *testing.T) {
+		actual := SumAll([]int{1, 2}, []int{0, 9})
+		expected := []int{3, 9}
+
+		if !slices.Equal(actual, expected) {
+			t.Errorf("expected %v got %v", expected, actual)
+		}
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("should sum tails only", func(t *testing.T) {
+		actual := SumAllTails([]int{1, 2}, []int{0, 9}, []int{5, 1, 2, 3}, []int{})
+		expected := []int{2, 9, 6, 0}
+
+		if !reflect.DeepEqual(actual, expected) {
+			t.Errorf("expected %v got %v", expected, actual)
 		}
 	})
 }
